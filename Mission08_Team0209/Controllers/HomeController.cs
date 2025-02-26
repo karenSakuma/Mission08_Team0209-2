783700a580ef184c3eee6704c2384c0ea9ea659e
karensakuma19@gmail.com <karensakuma19@gmail.com>
2025-02-26 11:42:47 -0700

    added constructor in controller

--- Mission08_Team0209/Controllers/HomeController.cs
+++ Mission08_Team0209/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Mission08_Team0209.Models;
 
 namespace Mission08_Team0209.Controllers;
 
 public class HomeController : Controller
 {
-    public HomeController()
+
+    private TaskCollectionContext _context;
+
+    public HomeController(TaskCollectionContext temp) //constructor
     {
-   
+        _context = temp;
     }
 
     public IActionResult Index()
