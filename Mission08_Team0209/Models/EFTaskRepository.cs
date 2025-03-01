58f8e7b49b6fd1f9d0bdf9b5559ad0f1786db06c
jm4567 <jenna.magleby@gmail.com>
2025-02-28 18:16:17 -0700

    added categories

--- Mission08_Team0209/Models/EFTaskRepository.cs
+++ Mission08_Team0209/Models/EFTaskRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Mission08_Team0209.Models;
 
 public class EfTaskRepository : ITaskRepository
@@ -8,18 +10,10 @@ public class EfTaskRepository : ITaskRepository
     {
         _context = temp;
     }
-    public List<Task> Tasks => _context.Tasks.ToList();
-
-    public Task? GetTaskById(int id)
-    {
-        return _context.Tasks.FirstOrDefault(t => t.TaskId == id);
-    }
-
-    public List<Category> GetCategories()
-    {
-        return _context.Categories.ToList();
-    }
-
+    public List<Task> Tasks => _context.Tasks.Include(t => t.Category).ToList();
+    
+    public List<Category> Categories => _context.Categories.ToList();
+    
     //add task 
     public void AddTask(Task task)
     {
