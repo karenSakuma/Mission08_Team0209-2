2640a3c63f994c5ad6f03ec3c67408b0b14aa677
karensakuma19@gmail.com <karensakuma19@gmail.com>
2025-02-28 17:00:10 -0700

    added to the controller page. added getCategories and getTaskId

--- Mission08_Team0209/Controllers/HomeController.cs
+++ Mission08_Team0209/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Mission08_Team0209.Models;
+using Task = Mission08_Team0209.Models.Task;
 
 namespace Mission08_Team0209.Controllers;
 
 public class HomeController : Controller
 {
-
     private ITaskRepository _repo;
 
     public HomeController(ITaskRepository temp) //constructor
@@ -20,4 +20,73 @@ public class HomeController : Controller
         return View();
     }
     
+    //[HttpGet]
+    public IActionResult AddEdit(int? id)
+    {
+        if (id == null)
+        {
+            // Add mode: Return an empty model
+            var model = new Task(); // New empty task model
+            ViewBag.Categories = _repo.GetCategories(); // Populate dropdown data using the repository
+            return View(model);
+        }
+        else
+        {
+            // Edit mode: Fetch the existing model
+            var model = _repo.GetTaskById(id.Value); // Use the repository to fetch the task by id
+            if (model == null)
+            {
+                return NotFound(); // Return a 404 if no task is found
+            }
+            ViewBag.Categories = _repo.GetCategories(); // Fetch categories again using the repository
+            return View(model);
+        }
+    }
+    
+    [HttpPost]
+    public IActionResult AddEdit(Task task)
+    {
+        if (ModelState.IsValid)
+        {
+            if (task.TaskId == 0)
+            {
+                // Add new task
+                _repo.AddTask(task);
+            }
+            else
+            {
+                // Update existing task
+                _repo.UpdateTask(task);
+            }
+
+            return RedirectToAction("Quadrants"); // Redirect to a list or details page after saving
+        }
+        
+        ViewBag.Categories = _repo.GetCategories();
+        return View(task);
+    }
+
+    
+/*    public IActionResult AddEdit(int? id)
+    {
+        if (id == null)
+        {
+            // Add mode: Return an empty model
+            var model = new Task();
+            ViewBag.Categories = _repo.GetCategories(); // Populate dropdown data
+            return View(model);
+        }
+        else
+        {
+            // Edit mode: Fetch the existing model
+            var model = _repo.GetTaskById(id.Value);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Categories = _repo.GetCategories();
+            return View(model);
+        }
+    }
+    */
 }
--- Mission08_Team0209/Models/ITaskRepository.cs
+++ Mission08_Team0209/Models/ITaskRepository.cs
@@ -5,6 +5,10 @@ namespace Mission08_Team0209.Models;
 public interface ITaskRepository
 {
     List<Task> Tasks { get; }
+    
+    Task? GetTaskById(int id); // Retrieve a task by ID
+    List<Category> GetCategories(); // Fetch categories
+    
     public void AddTask(Task task);
     
     public void UpdateTask(Task task);
